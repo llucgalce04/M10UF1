63a5816e81e03f16c4ef25916f7191cac661e436
Lluc Galceran <llucgalce@gmail.com>
2023-05-29 21:32:37 +0200

    Casi

--- Classe/medicine_view.sql
+++ Classe/medicine_view.sql
@@ -1,8 +1,8 @@
-DROP VIEW IF EXISTS total_cost_medicines;
-CREATE VIEW total_cost_medicines AS
-SELECT treatments.id_medicine, 
-COUNT(treatments.medicine) AS total_medicines,treatments.cost_production,
-SUM(treatments.cost_production * COUNT(treatments.medicine)) AS total_cost
-FROM medicines 
-LEFT JOIN treatments on medicines.id_medicine = treatments.id_medicine 
-GROUP BY treatments.id_medicine, treatments.cost_production;
+drop view costes;
+create view profit_medicine as select treatments.id_medicine,
+COUNT(medicine) medicines, cost,
+SUM(cost) total_cost_production, price,
+SUM(price) total_cost_sell from medicines LEFT JOIN
+treatments on medicines.id_medicine=treatments.id_medicine group
+by cost;
+
